cc1358576c203c5f6531493f8190c4f7f4753781
Artem Iarygin <a.iarygin@ffintech.com>
2026-02-10 00:07:06 +0400

    Initial Commit

---
+++ Grokk/GrokkApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrokkApp.swift
+//  Grokk
+//
+//  Created by Артем Ярыгин on 10.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct GrokkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
